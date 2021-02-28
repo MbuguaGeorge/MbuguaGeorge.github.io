e84f510caa26bd3a9a79623dc58ca34e89bab03b
unknown <mbuguag026@gmail.com>
2021-02-28 09:57:21 +0300

    made pages responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
-    if(scroll_position > 590){
-        trial.style.backgroundColor = '#1c1c1c'
+    if(scroll_position > 250){
+        trial.style.backgroundColor = '#1d1d1d'
         trial.style.transition = '0.25s'
     }
     else{
         trial.style.backgroundColor = 'transparent';
     }
 });
+const navSlide = () => {
+    const header = document.querySelector('.divs');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    header.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    
+
+    navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 1s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+         header.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
